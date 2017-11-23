0fd2f2c665a30ee2d8da2927380a496ba55785c6
Maxime Delplanque <maxime.delplanque@decathlon.com>
2017-11-23 17:12:12 +0100

    Add exo 2

--- 02_programming_fundamentals/03_algorithms_day_1/02_play_with_arrays/02_arrays_methods.js
+++ 02_programming_fundamentals/03_algorithms_day_1/02_play_with_arrays/02_arrays_methods.js
@@ -2,13 +2,28 @@
  ## Use some methods
  */
 
-// Rewrite your array `digits` using `push`;
-
+// Rewrite your array `digits` using `push`; // on peut faire aussi:  digits.p ush(0,1,2,3,4,5,6,7,8,9)
+// ou une boucle
+// for (let i = 0; i < 10; i++) {
+//    digits.p ush(i)
+//}
+const digits = [];
+digits.push(0);
+digits.push(1);
+digits.push(2);
+digits.push(3);
+digits.push(4);
+digits.push(5);
+digits.push(6);
+digits.push(7);
+digits.push(8);
+digits.push(9);
 
 // rewrite your variable `last` using `length`;
-
+const last = digits[digits.length-1];
 
 // create another array called `litteralDigits` from `zero` to `nine` where each array entry is a spelled-out number;
-
+const litteralDigits = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
 
 // use `join` to create, into the variable `allDigits`, a string like this : `zero - one ...`.
+const allDigits = litteralDigits.join(" - ");
