263d63b61c5978ef229e9afb68ab67f42c515cf0
Maxime Delplanque <maxime.delplanque@decathlon.com>
2017-11-24 16:51:01 +0100

    Add exo fizz_buzz

--- 02_programming_fundamentals/04_algorithms_day_2/03_fizz_buzz/fizz_buzz.js
+++ 02_programming_fundamentals/04_algorithms_day_2/03_fizz_buzz/fizz_buzz.js
@@ -9,8 +9,36 @@
 */
 
 function fizzBuzz(list) {
-
+  const newList = [];
+  for (let i = 0; i < list.length; i++) {
+    if (list[i] % 3 === 0 && list[i] % 5 === 0) {
+      newList[i] = "FizzBuzz";
+    } else if (list[i] % 5 === 0) {
+      newList[i] = "Buzz";
+    } else if (list[i] % 3 === 0) {
+      newList[i] = "Fizz";
+    } else {
+      newList[i] = list[i];
+    }
+  }
+  return newList;
 }
 
+// function range(startNumber, endNumber) {
+//   const array = [];
+//   if (startNumber < endNumber) {
+//     for (let i = startNumber; i <= endNumber; i++) {
+//       array.push(i);
+//     }
+//   } else {
+//     for (let i = startNumber; i >= endNumber ; i--) {
+//       array.push(i);
+//     }
+//   }
+//   return array;
+// }
+//
+// const array_test = range (1,20);
+// console.log(fizzBuzz(array_test));
 
 module.exports = fizzBuzz;
