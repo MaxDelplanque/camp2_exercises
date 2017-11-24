740cdfcd9132b8f0d86fc7430793776444fb94aa
Maxime Delplanque <maxime.delplanque@decathlon.com>
2017-11-24 15:30:27 +0100

    Add exo 2

--- 02_programming_fundamentals/04_algorithms_day_2/02_car_rental/01_index.js
+++ 02_programming_fundamentals/04_algorithms_day_2/02_car_rental/01_index.js
@@ -9,8 +9,19 @@ const driver = {
 // * Input: a driver
 // * Output: a boolean if the driver can rent a car
 
+function canRentACar (driver) {
+  if (
+    (driver.driverLicense === "B" || driver.driverLicense === "B1")
+    && driver.licenseIssued <= 2015
+    && (driver.numberOfAccident === 0 || driver.bonus >= 0.7)
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
-
+console.log(canRentACar(driver));
 
 // ⚠ Do not remove me ! It's for tests
 // eslint-disable-next-line
