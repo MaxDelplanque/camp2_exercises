7f617801990298b7da271810c93c0bffc4adce08
Maxime Delplanque <maxime.delplanque@decathlon.com>
2017-11-27 15:51:16 +0100

    Add exo filter partie 1 et 2

--- 02_programming_fundamentals/04_algorithms_day_2/04_filter/01_filter_with_string.js
+++ 02_programming_fundamentals/04_algorithms_day_2/04_filter/01_filter_with_string.js
@@ -1,21 +1,24 @@
 // filter takes an array of integer and a string with either odd or even
 // such as filter([1, 2, 3, 4, 5], 'even') returns [2, 4]
 function filter(array, str) {
-  const newArray = [];
+  let newArray = [];
   if (str === "even") {
     for ( let i = 0 ; i < array.length; i++){
-      if ( array[i] % 2 === 0 && array[i] !== 0) {
+      if ( array[i] % 2 === 0 ) {
         newArray.push(array[i]);
       }
     }
   }
   else if (str === "odd") {
     for ( let i = 0; i < array.length; i++) {
-      if (array[i] % 2 !== 0 || array[i] !== 0) {
+      if (array[i] % 2 !== 0) {
         newArray.push(array[i]);
       }
     }
-  } return newArray;
+  } else {
+    newArray = array;
+  }
+  return newArray;
 }
 
 filter([1, 2, 3, 4, 5], "even");
--- 02_programming_fundamentals/04_algorithms_day_2/04_filter/02_filter_with_function.js
+++ 02_programming_fundamentals/04_algorithms_day_2/04_filter/02_filter_with_function.js
@@ -1,8 +1,21 @@
 // filter takes an array of integer and a function of filtering
 // such as filter([1, 2, 3, 4, 5], pickEvenNumbers) returns [2, 4]
-function filter(array, fn) {
-  // Your code here
+
+function pickEvenNumbers(number) {
+  return number % 2 === 0;
 }
+
+function filter(array, fn) {
+  const newArray = [];
+  for ( let i = 0 ; i < array.length; i++){
+    if ( fn(array[i])) {
+      newArray.push(array[i]);
+    }
+  }
+  return newArray;
+}
+
+filter([1, 2, 3, 4, 5], pickEvenNumbers);
 
 // do not remove this line, it is for tests
 module.exports = filter;
