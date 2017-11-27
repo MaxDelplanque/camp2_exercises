6cdd8e63b3a25272f7283d671de5a125a6453548
Maxime Delplanque <maxime.delplanque@decathlon.com>
2017-11-27 18:05:59 +0100

    Add fizz_buzz_map exo

--- 02_programming_fundamentals/04_algorithms_day_2/06_fizz_buzz_with_map/fizz_buzz_map.js
+++ 02_programming_fundamentals/04_algorithms_day_2/06_fizz_buzz_with_map/fizz_buzz_map.js
@@ -9,9 +9,49 @@
    YOU MUST USE array.map
 */
 
-function fizzBuzz(list) {
-
+function doAFizzBuzz(number) {
+  if (number % 3 === 0 && number % 5 === 0) {
+    return "FizzBuzz";
+  } else if (number % 5 === 0) {
+    return "Buzz";
+  } else if (number % 3 === 0) {
+    return "Fizz";
+  } else {
+    return number;
+  }
 }
 
+function fizzBuzz(list) {
+  const newArray = list.map(doAFizzBuzz);
+  return newArray;
+}
+
+fizzBuzz([1, 2, 3, 4, 5, 6]);
 
 module.exports = fizzBuzz;
+
+// Exemple:
+//
+// function increment(number) {
+//   return number + 1;
+// }
+//
+// const nextArray = [1, 2, 3].map(increment);
+// nextArray === [2, 3, 4];
+//
+// function fizzBuzz(list) {
+//   const newList = [];
+//
+//   for (let i = 0; i < list.length; i++) {
+//     if (list[i] % 3 === 0 && list[i] % 5 === 0) {
+//       newList[i] = "FizzBuzz";
+//     } else if (list[i] % 5 === 0) {
+//       newList[i] = "Buzz";
+//     } else if (list[i] % 3 === 0) {
+//       newList[i] = "Fizz";
+//     } else {
+//       newList[i] = list[i];
+//     }
+//   }
+//   return newList;
+// }
