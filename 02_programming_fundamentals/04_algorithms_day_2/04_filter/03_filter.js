68ca50f9ceb9ecc05efc4cf9c14cbea781765ba5
Maxime Delplanque <maxime.delplanque@decathlon.com>
2017-11-27 16:58:20 +0100

    Add exo 03_filter

--- 02_programming_fundamentals/04_algorithms_day_2/04_filter/03_filter.js
+++ 02_programming_fundamentals/04_algorithms_day_2/04_filter/03_filter.js
@@ -1,8 +1,15 @@
 // filter takes an array of integer and a function of filtering by using an higher order function
 // such as filter([1, 2, 3, 4, 5], pickEvenNumbers) returns [2, 4]
-function filter(array, fn) {
-  // Your code here
+function pickEvenNumbers(number) {
+  return number % 2 === 0;
 }
+
+function filter(array, fn) {
+  const newArray = array.filter(fn);
+  return newArray;
+}
+
+filter([1, 2, 3, 4, 5], pickEvenNumbers)
 
 // do not remove this line, it is for tests
 module.exports = filter;
