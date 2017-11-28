e8761ec05873ea2b33f6ddc42dfbd73c761f6e8d
Maxime Delplanque <maxime.delplanque@decathlon.com>
2017-11-28 17:33:38 +0100

    Add exo memory incompleted

--- 02_programming_fundamentals/06_your_first_program/03_memory/memory.js
+++ 02_programming_fundamentals/06_your_first_program/03_memory/memory.js
@@ -1,2 +1,38 @@
 // This function will clear the terminal when called
 const clear = require("cli-clear");
+
+// The memory is a game where you have turned-over cards in front of you
+// and you must find pairs. You can return two cards at a time, if
+// they are the same, they stay visible, if not,
+// they go back to being flipped over.
+//
+// Instead of cards, we will be using emojis and you can start with these:
+//
+// const cards = ["🐰", "🐰", "🎃", "🎃", "🌲","🌲"];
+// For this progran, you will need to install packages in the exercise
+// directory. You do that with the command npm install.
+// It will allow you to use the clear() function that will clear
+// your terminal (you need to show only the cards you found).
+
+// Creation d'un Stream :
+const readline = require('readline');
+
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+const cards = ["🐰", "🐰", "🎃", "🎃", "🌲","🌲"];
+let returnedCards = [];
+
+
+function findPairs(answer) {
+  if (answer === "fine") {
+    console.log("Great!");
+    reader.close();
+  } else {
+    reader.question("How are you, really?", findPairs);
+  }
+}
+
+reader.question("Find the pairs!", findPairs);
