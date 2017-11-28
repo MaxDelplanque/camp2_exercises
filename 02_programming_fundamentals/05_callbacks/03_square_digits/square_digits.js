63a1383e7af9ca4949cdc68d07e21cc86a81a7ae
Maxime Delplanque <maxime.delplanque@decathlon.com>
2017-11-28 14:14:57 +0100

    Add square digits exo

--- 02_programming_fundamentals/05_callbacks/03_square_digits/square_digits.js
+++ 02_programming_fundamentals/05_callbacks/03_square_digits/square_digits.js
@@ -7,9 +7,24 @@
 // Note: This is not just the square of a number but the square of each digits
 // Note: The function accepts an integer and returns an integer
 
-function squareDigits(number) {
-  // Your code here
+function squareDigits(number){
+  let intToStr = number.toString(); //permet de transforme le nombre en chaîne de caractères
+  return parseInt(intToStr.split("").map(num => num*num).join(""), 10);
 }
+
+// La fonction parseInt() analyse une chaîne de caractère fournie en argument
+// et renvoie un entier exprimé dans une base donnée.
+
+// string.split : La méthode split() permet de diviser une chaîne de caractères
+// à partir d'un séparateur pour fournir un tableau de sous-chaînes.
+
+//La méthode map() crée un nouveau tableau composé des images des éléments du tableau courant
+//par une fonction donnée en argument.
+
+//La méthode join() réunit tous les éléments d'un tableau dans une chaine de caractères
+// et renvoie cette nouvelle chaîne de caractères.
+
+squareDigits(9129);
 
 // Do not remove last lines, it is for tests
 // eslint-disable-next-line
